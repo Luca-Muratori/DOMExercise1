3e10d253f35767feb4dc3a6840b7d7fa79ea9d42
Lucai994 <luca.muratori.94@hotmail.it>
2022-01-15 20:13:39 +0100

    done pther Dom exercise

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ const getFormvalue = function () {
     console.log(name + ' ' + surname)
 }
 
+//exercise 3
+const changeColor = function () {
+    const p = document.querySelector('.ex3')
+    const btn = document.querySelector('.ex3>button')
+    btn.addEventListener('click', function () {
+        p.style.backgroundColor = 'red'
+    })
+}
+changeColor()
+
+//exercise 4
+const getAttributes = function () {
+    const value = document.querySelector("#w3r")
+    const href = value.href
+    const rel = value.rel
+    alert('the rel is ' + href + ' and the href is ' + rel)
+}
+
+//exercise 5
+const insert_Row = function () {
+    const table = document.querySelector('#sampleTable');
+    const btn = document.querySelector("div.ex5 > input[type=button]")
+    btn.addEventListener('click', function () {
+        const newRow = table.insertRow(-1);
+        const newCellLeft = newRow.insertCell(0);
+        const newCellRight = newRow.insertCell(1);
+        let newText1 = document.createTextNode('New bottom row');
+        let newText2 = document.createTextNode('New bottom row');
+        newCellLeft.appendChild(newText1);
+        newCellRight.appendChild(newText2)
+    })
+
+}
